36e526e9799a61b85851006ac58e096e24eb4834
OTx4LqLgHWSty1VcrinzmFbotpMVeVyunTNW2hI <rttygfhhff@gmail.com>
2024-08-05 00:12:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,34 +6,30 @@
 #disableGuessImports = true
 #Go Tools > Dependencies > + Add new package > py-cord
 
+import os
 import discord #upm package(py-cord)
-
-import os
-
-
-# debug_guilds must not be set if we want to set contexts and integration_types on commands
-bot = discord.Bot()
-bot.load_extension('commands')
-
-import os
-import discord
 import asyncio
 from dotenv import load_dotenv
 from flask import Flask
 import threading
 
-app = Flask(__name__)
-
+# Çevresel değişkenleri yükle
 load_dotenv()
 
-bot = discord.Bot()
+# Flask uygulamasını oluştur
+app = Flask(__name__)
 
+# Discord botunu başlat
+bot = discord.Bot()
+bot.load_extension('commands')
+
+# Discord bot komutları
 @bot.slash_command(
     contexts={discord.InteractionContextType.private_channel},
     integration_types={discord.IntegrationType.user_install},
 )
-async def merhaba_de(ctx: discord.ApplicationContext, user: discord.User):
-    await ctx.respond(f"Merhaba Ben Eren Kara Namıdeğer xXxHileci Slayer - Wargods GuardxXx, {user}!")
+async def selamla(ctx: discord.ApplicationContext, user: discord.User):
+    await ctx.respond(f"Nabıyon Kardeş. Wgni Tarat Bakayım., {user}!")
 
 @bot.slash_command(
     integration_types={
@@ -41,24 +37,30 @@ async def merhaba_de(ctx: discord.ApplicationContext, user: discord.User):
         discord.IntegrationType.user_install,
     },
 )
-async def eren(ctx: discord.ApplicationContext):
+async def merhaba_eren(ctx: discord.ApplicationContext):
     await ctx.respond("Sanada Merhaba. Wargods Tarat Hadi")
 
 @bot.event
 async def on_ready():
     print(f'Bot {str(bot.user)} works')
 
+# Flask route tanımları
 @app.route('/')
 def index():
     return "Flask is working!"
 
+# Flask'ı çalıştıran iş parçacığı
 def run_flask():
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, use_reloader=False)
 
-async def start_bot():
-    await bot.start(os.getenv("DISCORD_TOKEN"))
+# Discord botunu çalıştıran ana fonksiyon
+def run_bot():
+    asyncio.run(bot.start(os.getenv("DISCORD_TOKEN")))
 
 if __name__ == "__main__":
+    # Flask'ı ayrı bir iş parçacığında başlat
     flask_thread = threading.Thread(target=run_flask)
     flask_thread.start()
-    asyncio.run(start_bot())
+
+    # Discord botunu başlat
+    run_bot()
