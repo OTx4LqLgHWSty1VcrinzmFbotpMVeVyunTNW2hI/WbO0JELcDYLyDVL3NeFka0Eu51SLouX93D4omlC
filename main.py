8e449cfd85c3ab18c545bc8edbc2ef102f1e47ef
OTx4LqLgHWSty1VcrinzmFbotpMVeVyunTNW2hI <rttygfhhff@gmail.com>
2024-08-09 10:38:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-    #Show Hidden Files
-    #Go To .replit [hidden]
-    #disableGuessImports = true
-    #entrypoint = "main.py"
-    #modules = ["python-3.10"]
-    #disableGuessImports = true
-    #Go Tools > Dependencies > + Add new package > py-cord
+#Show Hidden Files
+#Go To .replit [hidden]
+#disableGuessImports = true
+#entrypoint = "main.py"
+#modules = ["python-3.10"]
+#disableGuessImports = true
+#Go Tools > Dependencies > + Add new package > py-cord
 
 import os
 import discord #upm package(py-cord)
@@ -13,19 +13,15 @@ from dotenv import load_dotenv
 from flask import Flask
 import threading
 
-    # Çevresel değişkenleri yükle
 load_dotenv()
 
-    # Flask uygulamasını oluştur
 app = Flask(__name__)
 
-    # Discord botunu başlat
 bot = discord.Bot()
 bot.load_extension('commands')
 bot.load_extension('commands2')
 bot.load_extension('commands3')
 
-    # Discord bot komutları
 @bot.slash_command(
         contexts={discord.InteractionContextType.private_channel},
         integration_types={discord.IntegrationType.user_install},
@@ -51,11 +47,9 @@ async def on_ready():
 def index():
     return "Flask is working!"
 
-    # Flask'ı çalıştıran iş parçacığı
 def run_flask():
     app.run(host='0.0.0.0', port=5000, use_reloader=False)
 
-    # Discord botunu çalıştıran ana fonksiyon
 def run_bot():
     asyncio.run(bot.start(os.getenv("DISCORD_TOKEN")))
 
